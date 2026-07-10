8e505cb0869fa891e5a71365d0b7a358fc53b716
Vikram Latha <vikramlathavikram@gmail.com>
2026-07-10 17:10:17 +0530

    Connect HomeController with HomeService

--- src/main/java/com/gridweaver/gridweaver/controller/HomeController.java
+++ src/main/java/com/gridweaver/gridweaver/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.gridweaver.gridweaver.controller;
 
+import com.gridweaver.gridweaver.service.HomeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final HomeService homeService;
+
+    public HomeController(HomeService homeService) {
+        this.homeService = homeService;
+    }
+
     @GetMapping("/")
     public String home() {
-        return "Welcome to GridWeaver Backend!";
+        return homeService.getMessage();
     }
 }
